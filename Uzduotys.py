ca77abac1f3c1b155a5a6e7bbb2cdfd979fe0a02
Dominykas <dominykas.savickas@knf.stud.vu.lt>
2021-02-22 20:25:29 +0200

    Initial commit

---
+++ Uzduotys.py
@@ -0,0 +1,18 @@
+# TODO 1. Write a program that asks the user to enter comma-separated digits. Write data management exceptions. Using Python built-in functions output to the screen: sum of digits entered, maximum and minimum values, values which index is between 2 and 4, number of numbers entered, values greater than 10, list in ascending and descending order, and "inverted "list entered.
+# TODO 2. Sort the list by item length in ascending and descending order.
+'''python
+    sort_me = ["Kaunas", "Vilnius", "Alytus", "Klaipeda", "Varena", "Druskininkai", "Klaipeda"]
+'''
+# TODO 3. Perform list element transformation: 1. by using formula: x = x * (x - 10), 2. square each element.
+'''python
+    my_list = [12, 45, 23, 56, -546, 34]
+'''
+# TODO 4. Sort the dictionary according to the second item in the value list in ascending and descending order.
+'''python
+    sort_me = { 
+        "a": [7, 1, 9], 
+        "b": [8, -4, 3],
+        "c": [9, 10, 151],
+        "d": [-3, 9, 11]
+    }
+'''
